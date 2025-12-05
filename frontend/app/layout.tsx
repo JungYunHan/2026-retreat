3a74a6ea2433afe9becfcafa25bcaec111e3d942
JungYunHan <hanjy2008@naver.com>
2025-12-06 01:00:36 +0900

    feat: pretendard 폰트 적용

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import RootLayoutClient from '@/components/RootLayoutClient';
+import localFont from 'next/font/local';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+const pretendard = localFont({
+  src: './fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+  variable: '--font-pretendard',
 });
 
 export const metadata: Metadata = {
@@ -26,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang='ko'>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${pretendard.variable} ${pretendard.className} antialiased`}
       >
         <RootLayoutClient>{children}</RootLayoutClient>
       </body>
